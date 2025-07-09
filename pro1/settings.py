2717ba315a3559fd12fea819f7c0c102f96fc895
boopathychandran <boopathychandran300996@gmail.com>
2025-07-09 19:33:53 +0530

    Update settings.py

--- pro1/settings.py
+++ pro1/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2bxf-6nm#d^-6nkvwm)rttvu@vhso&6=)k48mr9y*jocw*c33@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['boopathychandran.pythonanywhere.com']
 
